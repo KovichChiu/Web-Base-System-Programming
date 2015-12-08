04b73f490ec9a2b67b94a0813ac9a608a246b47c
KovichChiu <kovich850329@gmail.com>
2015-12-08 20:47:31 +0800

    20151208
    Added "index" Frame

---
+++ Inc_Navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="#">KC Inc.</a>
+  <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Features</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Pricing</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About</a>
+    </li>
+  </ul>
+  <form class="form-inline navbar-form pull-right">
+    <input class="form-control" type="text" placeholder="Search">
+    <button class="btn btn-info-outline" type="submit">Search</button>
+  </form>
+</nav>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<?php 
+		include 'Head.php';
+	?>
+	<title> Web Course 2015 ver 0.1.0 by Kovich</title>
+	<body class="container">
+		<header style="padding: 20px 0px 0px 0px">
+			<h1 class="text-center"> Web Course 2015 <small> by Kovich <small> ver 0.1.0 </small> </small> </h1>
+			<hr>
+			<?php include 'Inc_Navbar.php'; ?>
+		</header>
+		<section style="padding: 20px 0px 0px 0px">
+				<!-- card 1 -->
+			<div class="card card-block">
+				<h4 class="card-title">Card title</h4>
+				<p class="card-text">
+					Some quick example text to build on the card title and make up the bulk of the card's content.
+				</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+			</div>
+				<!-- card 2 -->
+			<div class="card card-block">
+				<h4 class="card-title">Card title</h4>
+				<p class="card-text">
+					Some quick example text to build on the card title and make up the bulk of the card's content.
+				</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+			</div>
+				<!-- card 3 -->
+			<div class="card card-block">
+				<h4 class="card-title">Card title</h4>
+				<p class="card-text">
+					Some quick example text to build on the card title and make up the bulk of the card's content.
+				</p>
+				<a href="#" class="card-link">Card link</a>
+				<a href="#" class="card-link">Another link</a>
+			</div>
+		</section>
+		<footer style="padding: 20px 0px 0px 0px">
+			<hr>
+			<p class="text-center"><b> <i class="fa fa-cc"></i> 2015 KovichChiu, St. </b> </p>
+		</footer>
+	</body>
+</html>

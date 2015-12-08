ce95aeb94b19a966b58a928d0913608269858b42
KovichChiu <kovich850329@gmail.com>
2015-12-08 23:09:04 +0800

    20151208 ver. 0.1.1
    Added "Navbar Time<date>"
    Added "Mobile Devices<viewport>"

--- Inc_Navbar.php
+++ Inc_Navbar.php
@@ -1,9 +1,15 @@
+<?php
+  // echo $idsql;
+  date_default_timezone_set('Asia/Taipei');
+  $today = getdate(); //monday week begin reconvert
+
+  $d = ((intval($today['mday']) < 10)?"0":"").$today['mday'];
+  $m = ((intval($today['mon']) < 10)?"0":"").$today['mon'];
+  $y = $today['year'];
+?>
 <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="#">KC Inc.</a>
+  <a class="navbar-brand" href="index.php">KC Inc.</a>
   <ul class="nav navbar-nav">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
     <li class="nav-item">
       <a class="nav-link" href="#">Features</a>
     </li>
@@ -14,8 +20,9 @@
       <a class="nav-link" href="#">About</a>
     </li>
   </ul>
-  <form class="form-inline navbar-form pull-right">
-    <input class="form-control" type="text" placeholder="Search">
-    <button class="btn btn-info-outline" type="submit">Search</button>
-  </form>
+  <ul class="nav navbar-nav pull-right">
+    <li class="nav-item active">
+      <a class="nav-link" href=""><?php echo "$y/$m/$d"; ?></a>
+    </li>
+  </ul>
 </nav>

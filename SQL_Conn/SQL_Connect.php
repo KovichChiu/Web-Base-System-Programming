ffef156b7f6eae83b22eb7da7936e19041ed6dd2
KovichChiu <kovich850329@gmail.com>
2015-12-25 14:40:12 +0800

    Delete SQL_Connect.php

--- SQL_Conn/SQL_Connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$db_server = "localhost";
-	$db_name = "Kovich_DB";
-	$db_user = "kovich";
-	$db_pw = "jdes930108";
-	if(!@mysql_connect($db_server,$db_user,$db_pw)) die("connect failed");
-	mysql_query("SET NAMES utf8");
-	if(!@mysql_select_db($db_name)) die("can't use DB");
-?>
